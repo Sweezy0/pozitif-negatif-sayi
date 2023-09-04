f5d96f294f3189646e71fbd4ed14ca008350863b
Sweezy0 <119443953+Sweezy0@users.noreply.github.com>
2023-09-04 15:21:07 +0300

    Add files via upload

---
+++ pozitif_negatif_nötr.py
@@ -0,0 +1,7 @@
+sayi=int(input("bir sayı girin:"))
+if sayi>0:
+    print("{} sayısı pozitiftir".format(sayi))
+elif sayi==0:
+    print("{} sayısı nötrdür".format(sayi))
+else:
+    print("{} sayısı negatiftir".format(sayi))
